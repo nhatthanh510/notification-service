ed89ae5e0ba09844f7dfa059e47efafbc9f1f33d
Thanh Nguyen <nhatthanh510@gmail.com>
2023-02-28 18:51:47 +0700

    - Add authencation guard + validation pipeline

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { NotificationModule } from 'modules/notification/notification.module';
+import { AuthModule } from 'modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { NotificationModule } from 'modules/notification/notification.module';
       envFilePath: ['.env.dev', '.env.local'],
     }),
     NotificationModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/notification/notification.controller.ts
+++ src/modules/notification/notification.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { EmailRequestDTO } from './dto/email_request.dto';
 import { PushNotificationDTO } from './dto/push_notification.dto';
 import { SMSRequestDTO } from './dto/sms_request.dto';
@@ -9,16 +10,19 @@ export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
 
   @Post('email')
+  @UseGuards(AuthGuard('api-key'))
   sendEmail(@Body() requestData: EmailRequestDTO) {
     return this.notificationService.sendEmailToRecipients(requestData);
   }
 
   @Post('sms')
+  @UseGuards(AuthGuard('api-key'))
   sendSMS(@Body() requestData: SMSRequestDTO) {
     return this.notificationService.sendSMSToRecipients(requestData);
   }
 
   @Post('push_notification')
+  @UseGuards(AuthGuard('api-key'))
   sendPushNotification(@Body() requestData: PushNotificationDTO) {
     return this.notificationService.sendPushNotificationToRecipients(
       requestData,
